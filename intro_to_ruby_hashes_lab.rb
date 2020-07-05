043f6304342a4ee12e96c93aaa4cdfe36d9c29fd
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-05 23:16:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,10 +49,18 @@ railroads = {:railroads => {:pieces => 4,
       :four_pieces_owned => 200
     }, 
     :names => {
-      :reading_railroad => {},
-      :pennsylvania_railroad => {},
-      :b_and_o_railroad => {},
-      :shortline_railroad => {}
+      :reading_railroad => {
+        :mortgage_value => 100
+      },
+      :pennsylvania_railroad => {
+        :mortgage_value => 200
+      },
+      :b_and_o_railroad => {
+        :mortgage_value => 400
+      },
+      :shortline_railroad => {
+        :mortgage_value => 
+      }
     }
 }
 }
